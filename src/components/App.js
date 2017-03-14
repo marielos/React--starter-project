8345818b3733c2893404da8a90de34224c2c9ab1
Santiago Seira <sseira@pa-m-sseira.ideo.com>
2017-03-14 11:00:10 -0700

    react + node.js server

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,51 @@ import '../assets/stylesheets/base.scss';
 import React, { Component } from 'react';
 
 class App extends Component {
+
+
+
+  componentDidMount() {
+    this.setRouteStops();
+  }
+
+  setRouteStops() {
+    var routeStops = [
+      {
+        name: 'IDEO',
+        lat: 37.442309, 
+        lng: -122.160738
+      },
+      {
+        name: 'Med Wraps',
+        lat: 37.425994, 
+        lng: -122.144927
+      },
+      {
+        name: 'Stanford',
+        lat: 37.428057,
+        lng: -122.165364
+      }
+    ]
+
+    this.setState({
+      stops : routeStops
+    }) 
+  }
+
+
+
+
+
   render() {
+  	if (!this.state) return null
+  		
     return(
-      <h1>Hello, {this.props.name}!</h1>
+      	<div className='row'>
+          {this.state.stops.map( function(stop) {
+
+            return <div className='col-xs' key={stop.name}> {stop.name} </div>
+          }.bind(this))}
+        </div>
     )
   }
 };
