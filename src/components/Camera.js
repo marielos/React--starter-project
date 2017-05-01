71e3b7870f5e21f28be7b8cb0ee393f79b391bda
Santiago Seira <sseira@ideo.com>
2017-05-01 15:16:14 -0700

    using navigator.useragent

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -2,9 +2,7 @@ import '../assets/stylesheets/base.scss'
 import React, { Component } from 'react'
 
 var MobileDetect = require('mobile-detect'),
-md = new MobileDetect('Mozilla/5.0 (Linux; U; Android 4.0.3; en-in; SonyEricssonMT11i' +
-    ' Build/4.1.A.0.562) AppleWebKit/534.30 (KHTML, like Gecko)' +
-    ' Version/4.0 Mobile Safari/534.30')
+md = new MobileDetect(navigator.userAgent)
 
 
 
